0235abdc3c116d17a86c07e538a0f5a1245d4604
Shunsei <khan.sufyan.cs@gmail.com>
2019-12-07 19:12:39 -0500

    Now storing user data in firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
 	appId: "1:1035976801817:web:82c5f16d81d122f0abf488"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (err) {
+			console.log("error creating user", err.message);
+		}
+	}
+
+	return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
