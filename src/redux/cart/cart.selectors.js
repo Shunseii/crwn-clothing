cfb8aa56a3df3c0f16f2cc5d1526c4bca39f2deb
Shunsei <khan.sufyan.cs@gmail.com>
2019-12-18 22:07:21 -0500

    Added Checkout Page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,19 @@ export const selectCartItems = createSelector(
 	cart => cart.cartItems
 );
 
+export const selectHidden = createSelector(
+	[selectCart],
+	cart => cart.hidden
+);
+
+export const selectCartTotal = createSelector(
+	[selectCartItems],
+	cartItems => cartItems.reduce(
+		(accumulateVal, cartItem) => accumulateVal + cartItem.quantity * cartItem.price, 
+		0
+	)
+);
+
 export const selectCartItemsCount = createSelector(
 	[selectCartItems],
 	cartItems => cartItems.reduce(
