414baaddf3ad3d5078e190c2e600de1d03dc62b4
Shunsei <khan.sufyan.cs@gmail.com>
2019-12-29 19:19:43 -0500

    Connected to backend using axios post request with stripe payment information

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey = 'pk_test_AO9PFOyK8NjsYW1MVDlyKeTY00zm9QqYRP';
 
 	const onToken = token => {
-		console.log(token);
-		alert('Payment Successful');
+		axios({
+			url: 'payment',
+			method: 'post',
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		}).then(response => {
+			alert('Payment Successful');
+		}).catch(err => {
+			console.log('Payment error: ', JSON.parse(err));
+			alert('There was an issue with your payment. Please make sure you use the provided credit card.');
+		});
 	};
 
 	return (
 		<StripeCheckout 
 			label='Pay Now'
-			name='Clothing Site'
+			name='CRWN Clothing'
 			billingAddress
 			shippingAddress
 			image='https://svgshare.com/i/CUz.svg'
